84d15273206e1c8d3c2f7e04f75bee4aac71cd41
joacoss10 <joacomorelli1@gmail.com>
2025-05-07 19:37:41 -0300

    Backend

---
+++ src/test/java/com/example/recetarium/demo/RecetariumBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recetarium.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetariumBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
